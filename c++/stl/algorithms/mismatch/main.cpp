c5df3686864258b30515a126ca29d9f564ceee78
Ian Macaulay <ismacaul@gmail.com>
2016-12-21 20:48:35 -0700

    Played with std::mismatch

---
+++ c++/stl/algorithms/mismatch/main.cpp
@@ -0,0 +1,21 @@
+#define CATCH_CONFIG_MAIN
+#include "../../../catch/catch.hpp"
+
+#include <algorithm>
+
+TEST_CASE("std::mismatch") {
+    std::string s = "Was it a car or a cat I saw";
+
+    auto pair = std::mismatch(s.begin(), s.end(), s.rbegin());
+
+    REQUIRE(s.begin() == pair.first);
+    REQUIRE(s.rbegin() == pair.second);
+
+    std::string v1 = { 2, 3, 4, 5, 6, 7, 8 };
+    std::string v2 = { 2, 3, 4, 4, 6, 7, 8 };
+
+    auto pair2 = std::mismatch(v1.begin(), v1.end(), v2.begin(), v2.end());
+    REQUIRE(v1.begin()+3 == pair2.first);
+    REQUIRE(v2.begin()+3 == pair2.second);
+}
+
